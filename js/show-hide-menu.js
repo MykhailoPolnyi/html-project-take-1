8244153928a9790fad9cab179b9d7b0e26bbc2e4
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-08-27 21:55:45 +0300

    Added first version of sidebar, base css files, and some SVGs for site

---
+++ js/show-hide-menu.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+  document.getElementById("sidenav").style.display = "block";
+}
+
+function closeSidebar() {
+  document.getElementById("sidenav").style.display = "none";
+}
+
