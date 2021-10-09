94a2bd299c056729c1fa383d98580b2874d727c9
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-10-09 17:57:36 +0300

    Renamed some js files. Changed scripts connection in all html files

--- js/user_control.js
+++ js/user_control.js
@@ -1,45 +1,64 @@
-class User {
-	constructor(login, email, is_admin, is_web, is_mobile) {
-		this.login = login;
-		this.email = email;
-		this.is_admin = is_admin;
-		this.is_web = is_web;
-		this.is_mobile = is_mobile;
-	}
+const get_user_id = (user) => {return `user-${user.login}`;}
+const login = () => { return document.getElementById("login"); }
+const email = () => {return document.getElementById("email"); }
+const is_admin = () => { return document.getElementById("is-admin"); }
+const is_web = () => { return document.getElementById("web-user"); }
+const is_mobile = () => { return document.getElementById("mobile-user"); }
+
+function clear_inputs(){
+	login().value = "";
+	email().value = "";
+	is_admin().checked = false;
+	is_mobile().checked = false;
+	is_web().checked = false;
 }
 
-function add_to_table(new_user, table_id) {
+function set_users_counters() {
+	document.getElementById("mobile-users-counter").innerHTML = mobile_users_counter;
+	document.getElementById("web-users-counter").innerHTML = web_users_counter;
+}
 
-	let user_id = new_user.login;
+function add_to_table(new_user) {
 
-	const edit_button = `<button onclick="edit_user(${user_id})"><img src="../../images/svg/pencil-fill.svg" alt="Edit"></button>`
-	const delete_button = `<button onclick="edit_user(${user_id})"><img src="../../images/svg/trash-fill.svg" alt="Delete"></button>`
+	const user_table = document.getElementById("users-container");
+	const edit_button = `<button onclick="edit_user(${get_user_id(new_user)})"><img src="../../images/svg/pencil-fill.svg" alt="Edit"></button>`
+	const delete_button = `<button onclick="delete_user(${get_user_id(new_user)})"><img src="../../images/svg/trash-fill.svg" alt="Delete"></button>`
 
-	let user_template = `<tr id="${user_id}"> \
-	<td>${new_user.login}</td> \
-	<td>${new_user.email}</td> \
-	<td>${new_user.is_web}</td> \
-	<td>${new_user.is_mobile}</td> \
-	<td>${edit_button} ${delete_button}</td>\
+	let user_template = 
+	`<tr id="${get_user_id(new_user)}"> \
+		<td>${new_user.login}</td> \
+		<td>${new_user.email}</td> \
+		<td>${new_user.is_mobile}</td> \
+		<td>${new_user.is_web}</td> \
+		<td>${edit_button} ${delete_button}</td>\
 	</tr>`;
-	document.getElementById(table_id).innerHTML += user_template;
+
+	user_table.innerHTML += user_template;
 }
 
 function add_user() {
-	let login = document.getElementById("login").value;
-	let email = document.getElementById("email").value;
-	let is_admin = document.getElementById("is-admin").checked;
-	let is_web = document.getElementById("web-user").checked;
-	let is_mobile = document.getElementById("mobile-user").checked;
 
-	const new_user = new User(login, email, is_admin, is_web, is_mobile);
+	const new_user = {
+		login: login().value, 
+		email: email().value, 
+		is_admin: is_admin().checked, 
+		is_web: is_web().checked, 
+		is_mobile: is_mobile().checked
+	};
 
-	add_to_table(new_user, "users")
+	if(is_web().checked) {web_users_counter++;}
+	if(is_mobile().checked) {mobile_users_counter++;}
+
+	set_users_counters();
+	clear_inputs();
+
+	add_to_table(new_user)
 }
 
-function delete_user(user_id) {
+function delete_user(id) {
+	deleted_elem = document.getElementById(id);
+	document.removeChild(deleted_elem);
 }
 
-function edit_user(user_id) {
-
+function edit_user(id) {
 }
